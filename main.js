ea4e27094255ee7f104ea1b22c51a5c16694f4c1
giuseppedelcorso <Giuseppedecoboolean@gmail.com>
2023-10-26 14:50:38 +0200

    slavataggio

---
+++ main.js
@@ -0,0 +1,8 @@
+let kilometri = prompt( "numero kilometri che deve percorrere");
+let etaUtente = prompt("Inserisci la tua età");
+let prezzo = kilometri * (0.21)
+
+document.getElementById("prezzo").innerHTML = "il costo del biglietto é, " + prezzo;        
+
+
+let prezzo - (20) 
