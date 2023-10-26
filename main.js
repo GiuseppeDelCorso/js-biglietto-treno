6c3e5c60bfc66b6d6f3e5f3f4856d16a78c3923c
giuseppedelcorso <Giuseppedecoboolean@gmail.com>
2023-10-26 15:32:36 +0200

    salvataggio

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
 let kilometri = prompt( "numero kilometri che deve percorrere");
 let etaUtente = prompt("Inserisci la tua età");
 let prezzo = kilometri * (0.21)
-let prezzoScontato = prezzo - 20
 
-document.getElementById("prezzo").innerHTML = "il costo del biglietto é, " + prezzo;        
-
+document.getElementById("prezzo").innerHTML = "il costo del biglietto é, " + prezzo;
+document.getElementById("prezzo").innerHTML = "il costo del biglietto é, " + prezzoScontato;          
 
 if (etaUtente < 18){
-
     let prezzoScontato = prezzo - 20;
-
 }
